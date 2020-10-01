60e08f4a3507fcec2ee5a251eeeddb901fc58d47
Just1ce013 <50172138+Just1ce013@users.noreply.github.com>
2020-10-01 22:01:00 +0500

    Add files via upload

---
+++ demo-web_2/src/main/java/com/example/demoweb/com/example/demoweb/model/Post.java
@@ -0,0 +1,48 @@
+package com.example.demoweb.com.example.demoweb.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String text;
+    private Integer likes;
+    private Date creationDate;
+
+    public Post() {
+    }
+
+    public Post(Long id, String text, Date creationDate) {
+        this.text = text;
+        this.likes = 0;
+        this.creationDate = creationDate;
+        this.id = id;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public Integer getLikes() {
+        return this.likes;
+    }
+
+    public Date getCreationDate() {
+        return this.creationDate;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+}
